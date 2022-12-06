46622385c77c44c583402ab8a4f5dd1fdf68c790
khoi <dinhvinhkhoi@gmail.com>
2022-12-06 09:26:37 +0700

    Signed-off-by: khoi <dinhvinhkhoi@gmail.com>

--- demo-component/src/App.js
+++ demo-component/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import TodoApp from "./components/TodoApp";
 
 function App() {
-  return <div className="form"></div>;
+  return (
+    <div className="form">
+      <TodoApp></TodoApp>
+    </div>
+  );
 }
 
 export default App;
